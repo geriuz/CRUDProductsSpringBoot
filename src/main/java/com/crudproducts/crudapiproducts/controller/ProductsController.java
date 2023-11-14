0cdab20f10d4d7076ae09e8bcb27b4433d84aefe
Geriuz <volmarjose@gmail.com>
2023-11-13 20:39:52 -0500

    Se agrega la CORS

--- src/main/java/com/crudproducts/crudapiproducts/controller/ProductsController.java
+++ src/main/java/com/crudproducts/crudapiproducts/controller/ProductsController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1")
 public class ProductsController {
